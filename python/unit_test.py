5e8080221de611e3d88bd7baf7c407fa84589289
Sam Stump <samuel.stump@verizon.net>
2014-06-16 20:19:17 -0400

    add comments
    added a few comments, testing commits

--- python/unit_test.py
+++ python/unit_test.py
@@ -2,13 +2,13 @@ import combalg
 import time
 
 # power set
-
 a = [1,2,3]
 print 'power set',a,':'
 for t in combalg.powerset(a):
   print t
 print
-  
+
+# k subsets  
 b = [1,2,3,4,5]
 k = 3
 print k,'subsets of',b,':'
@@ -16,18 +16,21 @@ for t in combalg.all_k_subsets(b,k):
   print t
 print
 
+# random subsets
 n = 10
 print n,'random subsets of',b
 for i in xrange(n):
   print combalg.random_subset(b)
 print
 
+# random k subset
 c = range(100)
 print n,'random',k,'subsets of [',c[0],'...',c[len(c)-1],']'
 for i in xrange(n):
   print combalg.random_k_subset(c,k)
 print
 
+# compositions
 print '2 compositions of 6'
 for t in combalg.compositions(6,2):
   print t
